e528b5d0c98f2b0035315201fa4c849d2bba2a3f
kavinda1996 <gayashankavinda95@gmail.com>
2026-01-20 14:46:44 +0530

    createUser & getAllUsers methods implement

---
+++ src/main/java/edu/icet/controller/UserController.java
@@ -0,0 +1,25 @@
+package edu.icet.controller;
+
+import edu.icet.model.User;
+import edu.icet.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
